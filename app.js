f70f1d085b7cf01a588d2f8615f1207f9f0be507
Winner <rimchung1008@gmail.com>
2024-04-09 17:02:18 +0200

    123

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ passport.deserializeUser((user, done) => done(null, user));
 
 const samlStrategy = new SamlStrategy(
   {
-    entryPoint: 'https://login.salesforce.com/?so=bet123',
+    entryPoint: 'https://login.salesforce.com/?so=00D7R000005HjV2',
     issuer: 'https://saas-innovation-41572.my.salesforce.com',
     callbackUrl: 'https://bet123.ninja.com/api/auth/saml/callback',
     cert: `-----BEGIN CERTIFICATE-----
